540c07eb44cafba5e1a25eb3ee73fde8586b22fa
Adam Boyer <alexander.boyer75@gmail.com>
2021-05-09 21:48:50 -0500

    refactored the website to use a node webserver

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+
+const app = express()
+const port = process.env.PORT || 3000
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views locaiton
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialPath)
+
+// Setup static directory to serve
+app.use(express.static(path.join(__dirname, '../public')))
+
+
+app.get('', (req, res) => {
+    res.render('index')
+})
+
+app.listen(port, () => {
+    console.log("Server is listening on port " + port)
+})
